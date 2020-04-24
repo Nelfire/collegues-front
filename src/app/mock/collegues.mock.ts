a21b2671451c89e759ece28c2427b2cdbdd20c81
Girard Vincent <girvincent@gmail.com>
2020-04-24 10:21:43 +0200

    Update

--- src/app/mock/collegues.mock.ts
+++ src/app/mock/collegues.mock.ts
@@ -1,4 +1,4 @@
-import { Collegue } from '../models/collegue';
+import { Collegue } from '../models/Collegue';
 
 export class collegues_mock {
 	col = new Collegue('6Z5D6Z5EF2', 'DURANT', 'Jacouille', 'durant.jacouille@gmain.com', new Date('1993-01-01'), 'https://i.pinimg.com/originals/da/af/a3/daafa3dd8d9082513dbbff43a1e252ea.jpg');
