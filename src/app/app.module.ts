9c3eb4d7c398576c135e714b1038f643d90a77d5
Girard Vincent <girvincent@gmail.com>
2020-04-24 13:47:44 +0200

    Debut 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CollegueComponent } from './collegue/collegue.component';
+import { RechercheCollegueParNomComponent } from './recherche-collegue-par-nom/recherche-collegue-par-nom.component';
+import {RouterModule, Routes} from "@angular/router";
+import { ModifierCollegueComponent } from './modifier-collegue/modifier-collegue.component';
+import { ListerCollegueComponent } from './lister-collegue/lister-collegue.component';
 
+const appRoute: Routes
 @NgModule({
   declarations: [
     AppComponent,
-    CollegueComponent
+    CollegueComponent,
+    RechercheCollegueParNomComponent,
+    ModifierCollegueComponent,
+    ListerCollegueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute);
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CollegueComponent implements OnInit {
   
   @Input() col: Collegue;
+  @Input() message3: string;
 
   message = 'Bonjour';
   message2 = 'Test';
